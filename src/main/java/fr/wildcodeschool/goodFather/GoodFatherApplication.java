98f1449d9efa8648f04ff4062f8848e823cb3f66
Clément Bouakil <bouakil.clement.pro@gmail.com>
2019-08-01 09:31:38 +0200

    initialisation spring project

---
+++ src/main/java/fr/wildcodeschool/goodFather/GoodFatherApplication.java
@@ -0,0 +1,13 @@
+package fr.wildcodeschool.goodFather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodFatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodFatherApplication.class, args);
+	}
+
+}
