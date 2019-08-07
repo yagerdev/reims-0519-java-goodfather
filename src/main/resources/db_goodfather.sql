1d42ce4da7411253b4b7049c6710466d7e8744bf
Furkan Celik <c.furkan.contact@gmail.com>
2019-08-07 17:46:45 +0200

    change password

--- src/main/resources/db_goodfather.sql
+++ src/main/resources/db_goodfather.sql
@@ -1,8 +1,4 @@
-BEGIN TRANSACTION;
-
 create DATABASE goodfather_db;
 CREATE USER 'goodfather'@'localhost' IDENTIFIED BY 'motdepasse';
 GRANT ALL ON goodfather_db.* TO 'goodfather'@'localhost';
 FLUSH PRIVILEGES;
-
-COMMIT;
