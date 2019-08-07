538c00933f5b2f9b2e99b204d1368ce7a8bd52b1
Clément Bouakil <bouakil.clement.pro@gmail.com>
2019-08-07 17:15:30 +0200

    added attribute + constructor + getters + setters

--- src/main/java/fr/wildcodeschool/goodFather/entities/Project.java
+++ src/main/java/fr/wildcodeschool/goodFather/entities/Project.java
@@ -1,7 +1,74 @@
 package fr.wildcodeschool.goodFather.entities;
 
+import java.sql.Date;
+
 public class Project {
 
-    
+    private int id;
+    private String name;
+    private String address;
+    private String city;
+    private int postalCode;
+    private Date createDate;
+
+    public Project() {
+    }
+
+    public Project(int id, String name, String address, String city, int postalCode, Date createDate) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.city = city;
+        this.postalCode = postalCode;
+        this.createDate = createDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public int getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(int postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
     
 }
