218da1dc39cb94ffbf7cab6b32cdeea778db6747
Youssef Sabouni <youssefsabouni@icloud.com>
2019-08-08 15:38:40 +0200

    renamed route controllers

--- src/main/java/fr/wildcodeschool/goodFather/controllers/ProjectController.java
+++ src/main/java/fr/wildcodeschool/goodFather/controllers/ProjectController.java
@@ -9,21 +9,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 public class ProjectController {
 
-    @GetMapping("/profil")
-    public String showProfil() {
-        return "profil";
-    }
-    
-    @PostMapping("/profil")
-    public String createProject(@RequestParam String name, @RequestParam String address, @RequestParam String city, @RequestParam String postalCode) {
-        Project project = new Project(name, address, city, postalCode);
-        ProjectRepository.save(project);
-        return "redirect:/project";
-    }
-
     @GetMapping("/project")
     public String showProject() {
         return "project";
     }
     
+    @PostMapping("/project")
+    public String createProject(@RequestParam String name, @RequestParam String address, @RequestParam String city, @RequestParam String postalCode) {
+        Project project = new Project(name, address, city, postalCode);
+        ProjectRepository.save(project);
+        return "redirect:/rooms";
+    }
+
 }
