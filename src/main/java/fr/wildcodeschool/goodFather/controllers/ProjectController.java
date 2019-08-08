9d355cb4ce5dd46bbe42abc6fd3a7be9baf34cb0
Youssef Sabouni <youssefsabouni@icloud.com>
2019-08-08 17:59:03 +0200

    correction of route controller

--- src/main/java/fr/wildcodeschool/goodFather/controllers/ProjectController.java
+++ src/main/java/fr/wildcodeschool/goodFather/controllers/ProjectController.java
@@ -4,21 +4,35 @@ import fr.wildcodeschool.goodFather.entities.Project;
 import fr.wildcodeschool.goodFather.repositories.ProjectRepository;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 public class ProjectController {
-
-    @GetMapping("/projects")
-    public String showProject() {
-        return "project";
-    }
     
-    @PostMapping("/projects")
+    @PostMapping("/projects/create")
     public String createProject(@RequestParam String name, @RequestParam String address, @RequestParam String city, @RequestParam String postalCode) {
         Project project = new Project(name, address, city, postalCode);
         ProjectRepository.save(project);
-        return "redirect:/rooms";
+        return "redirect:/project/id/edit";
     }
+
+    @GetMapping("/projects/create")
+    public String showProject() {
+        return "project-create";
+    }
+
+    @GetMapping("project/{id}/edit")
+    public String showCatetories(@PathVariable("id") Long projectId, @RequestParam Long categoryId){
+        if(categoryId == null) {
+            return "categories";
+        }
+        else {
+            return "redirect:/rooms/create?projectId={categoryId}";
+        }
+    }
+    
     
 }
