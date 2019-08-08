9e309e83ffa91ab41127457caf6d52feb0d97c2c
Youssef Sabouni <youssefsabouni@icloud.com>
2019-08-08 11:38:13 +0200

    added route profil & project

--- src/main/java/fr/wildcodeschool/goodFather/controllers/ProjectController.java
+++ src/main/java/fr/wildcodeschool/goodFather/controllers/ProjectController.java
@@ -1,18 +1,29 @@
 package fr.wildcodeschool.goodFather.controllers;
 
+import fr.wildcodeschool.goodFather.entities.Project;
+import fr.wildcodeschool.goodFather.repositories.ProjectRepository;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public class ProjectController {
 
     @GetMapping("/profil")
-    public String show() {
+    public String showProfil() {
         return "profil";
     }
     
     @PostMapping("/profil")
-    public String create() {
-        return "redirect:/profil";
+    public String createProject(@RequestParam String name, @RequestParam String address, @RequestParam String city, @RequestParam String postalCode) {
+        Project project = new Project(name, address, city, postalCode);
+        ProjectRepository.save(project);
+        return "redirect:/project";
     }
 
+    @GetMapping("/project")
+    public String showProject() {
+        return "project";
+    }
+    
 }
