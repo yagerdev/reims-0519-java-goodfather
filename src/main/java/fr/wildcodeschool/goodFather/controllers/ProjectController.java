2bc3faee09465f0255a025a3d975959eb9eb52ad
Youssef Sabouni <youssefsabouni@icloud.com>
2019-08-09 10:42:19 +0200

    Added annotation RestController

--- src/main/java/fr/wildcodeschool/goodFather/controllers/ProjectController.java
+++ src/main/java/fr/wildcodeschool/goodFather/controllers/ProjectController.java
@@ -3,19 +3,24 @@ package fr.wildcodeschool.goodFather.controllers;
 import fr.wildcodeschool.goodFather.entities.Project;
 import fr.wildcodeschool.goodFather.repositories.ProjectRepository;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestBody;
 
-
+@RestController
 public class ProjectController {
     
+    @Autowired
+    ProjectRepository projectRepository;
+
     @PostMapping("/projects/create")
     public String createProject(@RequestParam String name, @RequestParam String address, @RequestParam String city, @RequestParam String postalCode) {
         Project project = new Project(name, address, city, postalCode);
-        ProjectRepository.save(project);
+        projectRepository.save(project);
         return "redirect:/project/id/edit";
     }
 
