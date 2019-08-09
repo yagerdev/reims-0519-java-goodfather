a14035ca6aec706ae70cb562efe2e4421369c3db
Rémy Trohel <remy.trohel@gmail.com>
2019-08-09 11:04:06 +0200

    RestController changed to Controller

--- src/main/java/fr/wildcodeschool/goodFather/controllers/CategoryController.java
+++ src/main/java/fr/wildcodeschool/goodFather/controllers/CategoryController.java
@@ -7,9 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.Controller;
 
-@RestController
+@Controller
 public class CategoryController {
 
     @Autowired
