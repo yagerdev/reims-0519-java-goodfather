7c79a9d8f3037dac05f6858292485af5331fae21
Furkan Celik <c.furkan.contact@gmail.com>
2019-08-12 15:18:24 +0200

    add config folder

---
+++ src/main/java/fr/wildcodeschool/goodFather/config/MySecurityConfig.java
@@ -0,0 +1,16 @@
+package fr.wildcodeschool.goodFather.config;
+
+
+
+import org.springframework.boot.autoconfigure.security.servlet.WebSecurityEnablerConfiguration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@EnableWebMvc
+public class MySecurityConfig extends WebMvcConfigurerAdapter{
+    
+
+}
+
+
+}
