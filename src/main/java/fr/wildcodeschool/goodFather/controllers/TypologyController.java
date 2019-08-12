204bde6b47a36f372b010495a736a079061ce0b7
Clément Bouakil <bouakil.clement.pro@gmail.com>
2019-08-12 13:08:51 +0200

    added list typo in getmapping method + imports

--- src/main/java/fr/wildcodeschool/goodFather/controllers/TypologyController.java
+++ src/main/java/fr/wildcodeschool/goodFather/controllers/TypologyController.java
@@ -3,8 +3,11 @@ package fr.wildcodeschool.goodFather.controllers;
 import fr.wildcodeschool.goodFather.entities.Typology;
 import fr.wildcodeschool.goodFather.repositories.TypologyRepository;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,8 +19,10 @@ public class TypologyController {
     TypologyRepository typologyRepository;
 
     @GetMapping("/typologies")
-    public String showTypologies() {
+    public String showTypologies(Model model) {
         //Todo : check if user is admin
+        List<Typology> typologyList = typologyRepository.findAll();
+        model.addAttribute("typologies", typologyList);
         return "admin-typology";
     }
 
