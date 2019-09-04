afe0eefaacf4170f15d0103f575a19f91e899f68
Youssef Sabouni <youssefsabouni@icloud.com>
2019-09-04 16:48:08 +0200

    added TaskRepository

---
+++ src/main/java/fr/wildcodeschool/goodFather/repositories/TaskRepository.java
@@ -0,0 +1,11 @@
+package fr.wildcodeschool.goodFather.repositories;
+
+import fr.wildcodeschool.goodFather.entities.Task;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Long, Task> {
+
+}
