3bd69312cff8cf15eb754122d8a162eabbe974ba
Clément Bouakil <bouakil.clement.pro@gmail.com>
2019-09-04 15:06:21 +0200

    split viewController

--- src/main/java/fr/wildcodeschool/goodFather/controllers/IndexController.java
+++ src/main/java/fr/wildcodeschool/goodFather/controllers/IndexController.java
@@ -4,11 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class ViewController {
+public class IndexController {
 
-    @GetMapping("/login")
-    public String login() {
-        return "redirect:/";
+    @GetMapping("/")
+    public String index() {
+        return "index";
     }
 
 }
