ef98ad8abaf862d225818b7d7c4fb131a12bb741
Clément Bouakil <bouakil.clement.pro@gmail.com>
2019-09-04 16:28:55 +0200

    added mapping for footer fragment

--- src/main/java/fr/wildcodeschool/goodFather/controllers/FragmentsController.java
+++ src/main/java/fr/wildcodeschool/goodFather/controllers/FragmentsController.java
@@ -1,8 +1,14 @@
 package fr.wildcodeschool.goodFather.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class FragmentsController {
+
+    @GetMapping("/footer")
+    public String getFooter() {
+        return "footer";
+    }
 
 }
