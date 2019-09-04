57fa1f4ca23f3a9049fd3dc22539ddb4b8b446ea
Clément Bouakil <bouakil.clement.pro@gmail.com>
2019-09-04 14:25:19 +0200

    adding material controller with postmapping and getmapping for admin section

---
+++ src/main/java/fr/wildcodeschool/goodFather/controllers/MaterialController.java
@@ -0,0 +1,34 @@
+package fr.wildcodeschool.goodFather.controllers;
+
+import fr.wildcodeschool.goodFather.entities.Material;
+import fr.wildcodeschool.goodFather.repositories.MaterialRepository;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MaterialController {
+
+    @Autowired
+    MaterialRepository materialRepository;
+
+    @GetMapping("/materials")
+    public String showMaterials(Model model) {
+        List<Material> materialList = materialRepository.findAll();
+        model.addAttribute("materials", materialList);
+        return "admin-material";
+    }
+
+    @PostMapping("/materials")
+    public String createMaterial(@RequestParam String name) {
+        Material material = new Material(name);
+        materialRepository.save(material);
+        return "redirect:/materials";
+    }
+}
