e4e1449620d22be4ec255ba91dfc5da78ec70d2c
Clément Bouakil <bouakil.clement.pro@gmail.com>
2019-09-04 11:22:53 +0200

    added constructors

--- src/main/java/fr/wildcodeschool/goodFather/entities/Work.java
+++ src/main/java/fr/wildcodeschool/goodFather/entities/Work.java
@@ -1,8 +1,31 @@
 package fr.wildcodeschool.goodFather.entities;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Work {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column
+    private String name;
+
+    public Work() {
+    }
+
+    public Work(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Work(String name) {
+        this.name = name;
+    }
 
 }
