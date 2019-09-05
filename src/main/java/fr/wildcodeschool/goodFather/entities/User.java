853db85b753e95621bd54158152159b878c0d9ca
Youssef Sabouni <youssefsabouni@icloud.com>
2019-09-05 15:24:50 +0200

    fix id identation bdd

--- src/main/java/fr/wildcodeschool/goodFather/entities/User.java
+++ src/main/java/fr/wildcodeschool/goodFather/entities/User.java
@@ -20,7 +20,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 public class User implements UserDetails {
     
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(length = 100)
--- src/main/java/fr/wildcodeschool/goodFather/entities/Work.java
+++ src/main/java/fr/wildcodeschool/goodFather/entities/Work.java
@@ -1,15 +1,18 @@
 package fr.wildcodeschool.goodFather.entities;
 
+import java.util.Set;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 @Entity
 public class Work {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column
