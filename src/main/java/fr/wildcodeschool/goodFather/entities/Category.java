3ae4fe2dae4b40edaf9ed9cbaa1d4f495d94fa36
Youssef Sabouni <youssefsabouni@icloud.com>
2019-09-05 14:31:50 +0200

    fix bug id identation

--- src/main/java/fr/wildcodeschool/goodFather/entities/Category.java
+++ src/main/java/fr/wildcodeschool/goodFather/entities/Category.java
@@ -13,7 +13,7 @@ import javax.persistence.OneToMany;
 public class Category {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(length = 30)
--- src/main/java/fr/wildcodeschool/goodFather/entities/Task.java
+++ src/main/java/fr/wildcodeschool/goodFather/entities/Task.java
@@ -14,7 +14,7 @@ import javax.persistence.ManyToOne;
 public class Task {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private double price;
