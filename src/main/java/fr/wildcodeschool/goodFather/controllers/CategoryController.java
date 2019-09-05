86a6c21dd630072dd3e7a1ce042f543d73b6cf2b
Clément Bouakil <bouakil.clement.pro@gmail.com>
2019-09-05 18:10:36 +0200

    ok

--- src/main/java/fr/wildcodeschool/goodFather/controllers/CategoryController.java
+++ src/main/java/fr/wildcodeschool/goodFather/controllers/CategoryController.java
@@ -8,7 +8,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -32,5 +34,10 @@ public class CategoryController {
         return "redirect:/categories";
     }
 
+    @DeleteMapping("/categories/{id}")
+    public boolean deleteCategory(@PathVariable Long id){
+        categoryRepository.deleteById(id);
+        return true;
+    }
     
 }
