810d9e039eca37ee01ebbe84a7ee6602c54b9f64
Clément Bouakil <bouakil.clement.pro@gmail.com>
2019-09-06 14:51:30 +0200

    adding deletemapping in all controllers

--- src/main/java/fr/wildcodeschool/goodFather/controllers/UserController.java
+++ src/main/java/fr/wildcodeschool/goodFather/controllers/UserController.java
@@ -7,7 +7,9 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -37,6 +39,12 @@ public class UserController {
         ) { 
             User user = new User(firstName,lastName,email,phoneNumber, address,city,postalCode,password,role);
             userRepository.save(user);     
+        return "redirect:/users";
+    }
+
+    @DeleteMapping("/users/{id}")
+    public String deleteUser(@PathVariable Long id){
+        userRepository.deleteById(id);
         return "redirect:/users";
     }
 
