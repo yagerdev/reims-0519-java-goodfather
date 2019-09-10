ee071c9a5fec79db9093e6ce9d85073be56863e0
Rémy Trohel <remy.trohel@gmail.com>
2019-09-10 14:09:44 +0200

    PutMapping created to link typologies to one category

--- src/main/java/fr/wildcodeschool/goodFather/controllers/CategoryController.java
+++ src/main/java/fr/wildcodeschool/goodFather/controllers/CategoryController.java
@@ -5,7 +5,9 @@ import fr.wildcodeschool.goodFather.entities.Typology;
 import fr.wildcodeschool.goodFather.repositories.CategoryRepository;
 import fr.wildcodeschool.goodFather.repositories.TypologyRepository;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -42,6 +45,19 @@ public class CategoryController {
         model.addAttribute("listType", "typologies");
         model.addAttribute("myList", typologies);
         return "admin/config";
+    }
+
+    @PutMapping("/categories/{id}")
+    public String editCategory(@PathVariable(name = "id") Long categoryId, @RequestParam(name = "typologies") List<Long> typologyIds) {
+        Set<Typology> typologies = new HashSet<Typology>();
+        for (Long typologyId : typologyIds) {
+            Typology typology = typologyRepository.findById(typologyId).get();
+            typologies.add(typology);
+        }
+        Category category = categoryRepository.findById(categoryId).get();
+        category.setTypologies(typologies);
+        categoryRepository.save(category);
+        return "redirect:/categories";
     }
 
     @PostMapping("/categories")
