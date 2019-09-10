84f2e2b1f6ea9d6234f3b56d1757f739aac07848
Rémy Trohel <remy.trohel@gmail.com>
2019-09-10 16:19:49 +0200

    remove unused imports

--- src/main/java/fr/wildcodeschool/goodFather/entities/Category.java
+++ src/main/java/fr/wildcodeschool/goodFather/entities/Category.java
@@ -3,14 +3,12 @@ package fr.wildcodeschool.goodFather.entities;
 import java.util.HashSet;
 import java.util.Set;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 
