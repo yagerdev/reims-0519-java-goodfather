6d2fddfed0121706be0b1bf2f8a1b6d47bb47e88
Youssef Sabouni <youssefsabouni@icloud.com>
2019-09-11 12:55:09 +0200

    clean useless @JoinColumn annontation

--- src/main/java/fr/wildcodeschool/goodFather/entities/Typology.java
+++ src/main/java/fr/wildcodeschool/goodFather/entities/Typology.java
@@ -8,7 +8,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.ManyToMany;
 
 @Entity
@@ -22,11 +21,9 @@ public class Typology {
     private String name;
 
     @ManyToMany
-    @JoinColumn
     private Set<Task> tasks = new HashSet<>();
 
     @ManyToMany
-    @JoinColumn
     private Set<Category> categories = new HashSet<>();
 
     public Typology() {
