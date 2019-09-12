1c68c3ca89313808460472428c764ec093efb19d
Clément Bouakil <bouakil.clement.pro@gmail.com>
2019-09-12 17:02:49 +0200

    adding delete method for rooms

--- src/main/java/fr/wildcodeschool/goodFather/controllers/RoomController.java
+++ src/main/java/fr/wildcodeschool/goodFather/controllers/RoomController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,10 +13,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import fr.wildcodeschool.goodFather.entities.Material;
+import fr.wildcodeschool.goodFather.entities.Project;
 import fr.wildcodeschool.goodFather.entities.Room;
 import fr.wildcodeschool.goodFather.entities.Typology;
 import fr.wildcodeschool.goodFather.entities.Work;
 import fr.wildcodeschool.goodFather.repositories.MaterialRepository;
+import fr.wildcodeschool.goodFather.repositories.ProjectRepository;
 import fr.wildcodeschool.goodFather.repositories.RoomRepository;
 import fr.wildcodeschool.goodFather.repositories.TypologyRepository;
 import fr.wildcodeschool.goodFather.repositories.WorkRepository;
@@ -34,6 +37,9 @@ public class RoomController {
 
     @Autowired
     MaterialRepository materialRepository;
+
+    @Autowired
+    ProjectRepository projectRepository;
     
     @GetMapping("/rooms/create")
     public String showCreateRoom(
@@ -69,5 +75,13 @@ public class RoomController {
         model.addAttribute("typologies", typologyList);
         model.addAttribute("works", workList);
         return "tasks";
+    }
+
+    @DeleteMapping("/rooms/{id}")
+    public String delete(@PathVariable Long id, Model model) {
+        Room room = roomRepository.findById(id).get();
+        Long projectId = room.getProject().getId();
+        roomRepository.deleteById(id);
+        return "redirect:/projects/"+projectId+"/read";
     }
 }
