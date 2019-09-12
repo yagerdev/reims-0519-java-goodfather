49237b02f97c27a3a943595cee9e44ce11a79c2c
Furkan Celik <c.furkan.contact@gmail.com>
2019-09-12 11:49:55 +0200

    remove old param

--- src/main/java/fr/wildcodeschool/goodFather/controllers/IndexController.java
+++ src/main/java/fr/wildcodeschool/goodFather/controllers/IndexController.java
@@ -1,9 +1,6 @@
 package fr.wildcodeschool.goodFather.controllers;
 
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
@@ -11,12 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class IndexController {
 
     @GetMapping("/")
-    public String index(Model model) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        Object isLogged = authentication.getPrincipal();
-        Boolean bool = isLogged.getClass() == fr.wildcodeschool.goodFather.entities.User.class;
-        model.addAttribute("isLogged", bool);
-        System.out.println(isLogged.getClass().toString());
+    public String index() {
         return "index";
     }
 
