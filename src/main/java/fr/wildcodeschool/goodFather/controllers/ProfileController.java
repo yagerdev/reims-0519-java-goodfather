c5c894519f5a67e72f5c14499e25a966513c3214
Clément Bouakil <bouakil.clement.pro@gmail.com>
2019-09-17 15:33:39 +0200

    fix problem of password during edit profile and adding alert

--- src/main/java/fr/wildcodeschool/goodFather/controllers/ProfileController.java
+++ src/main/java/fr/wildcodeschool/goodFather/controllers/ProfileController.java
@@ -2,12 +2,15 @@ package fr.wildcodeschool.goodFather.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import fr.wildcodeschool.goodFather.entities.User;
 import fr.wildcodeschool.goodFather.repositories.ProjectRepository;
@@ -21,17 +24,22 @@ public class ProfileController {
 
     @Autowired
     ProjectRepository projectRepository;
-    
+
     @GetMapping("/profile")
-    public String loadProfile( Model model) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        User users = (User)authentication.getPrincipal();
-        model.addAttribute("user", users);
+    public String loadProfile(
+        Model model, 
+        Authentication authentication,
+        @RequestParam(value = "message", required = false) String message
+    ) {
+        User userToUpdate = (User) authentication.getPrincipal();
+        model.addAttribute("user", userToUpdate);
+        model.addAttribute("message", message);
         return "profile";
     }
 
     @PutMapping("/profile/{id}")
-    public String update(@PathVariable Long id, User user) {
+    public String update(@PathVariable Long id, User user, RedirectAttributes redirectAttributes) {
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
         User userToUpdate = userRepository.findById(id).get();
         userToUpdate.setFirstName(user.getFirstName());
         userToUpdate.setLastName(user.getLastName());
@@ -39,8 +47,10 @@ public class ProfileController {
         userToUpdate.setPhoneNumber(user.getPhoneNumber());
         userToUpdate.setAddress(user.getAddress());
         userToUpdate.setCity(user.getCity());
+        userToUpdate.setPassword(encoder.encode(user.getPassword()));
         userToUpdate.setPostalCode(user.getPostalCode());
         userRepository.save(userToUpdate);
+        redirectAttributes.addAttribute("message", "edit");
         return "redirect:/profile";
     }
 
