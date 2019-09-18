53a2638ed34691ba556889c59cf140050391c0f6
Clément Bouakil <bouakil.clement.pro@gmail.com>
2019-09-18 11:07:23 +0200

    remove useless import

--- src/main/java/fr/wildcodeschool/goodFather/controllers/ProfileController.java
+++ src/main/java/fr/wildcodeschool/goodFather/controllers/ProfileController.java
@@ -5,7 +5,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
