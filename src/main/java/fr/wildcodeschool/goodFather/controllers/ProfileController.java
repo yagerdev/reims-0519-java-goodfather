65b56a4b15cdaed04632960c58f4043f3dece718
Clément Bouakil <bouakil.clement.pro@gmail.com>
2019-09-18 09:57:57 +0200

    remove useless imports

--- src/main/java/fr/wildcodeschool/goodFather/controllers/ProfileController.java
+++ src/main/java/fr/wildcodeschool/goodFather/controllers/ProfileController.java
@@ -2,8 +2,6 @@ package fr.wildcodeschool.goodFather.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
