942c39d3027834656567f0a9d60d11b42ac70b1c
Furkan Celik <c.furkan.contact@gmail.com>
2019-09-24 14:03:00 +0200

    add new query to repository for delete task by id

--- src/main/java/fr/wildcodeschool/goodFather/repositories/TaskRepository.java
+++ src/main/java/fr/wildcodeschool/goodFather/repositories/TaskRepository.java
@@ -8,4 +8,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
     Task findTaskByWorkIdAndMaterialId(Long workId, Long materialId);
+	Task deleteById(Task taskId);
 }
