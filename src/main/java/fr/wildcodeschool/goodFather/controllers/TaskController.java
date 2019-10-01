bc22e48830c232b7ff60c44b1e218db6645089d5
Rémy Trohel <remy.trohel@gmail.com>
2019-10-01 23:38:11 +0200

    every new Task created by the admin will now duplicate for every Partner in order to allow them to set their own prices, adding a task in a room will now select a specific source of price, deleting a task will also delete copies of that task for every Partner

--- src/main/java/fr/wildcodeschool/goodFather/controllers/TaskController.java
+++ src/main/java/fr/wildcodeschool/goodFather/controllers/TaskController.java
@@ -4,6 +4,7 @@ import java.util.Collections;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -21,6 +22,7 @@ import fr.wildcodeschool.goodFather.entities.Quantity;
 import fr.wildcodeschool.goodFather.entities.Room;
 import fr.wildcodeschool.goodFather.entities.Task;
 import fr.wildcodeschool.goodFather.entities.Typology;
+import fr.wildcodeschool.goodFather.entities.User;
 import fr.wildcodeschool.goodFather.entities.Work;
 import fr.wildcodeschool.goodFather.repositories.MaterialRepository;
 import fr.wildcodeschool.goodFather.repositories.ProjectRepository;
@@ -28,6 +30,7 @@ import fr.wildcodeschool.goodFather.repositories.QuantityRepository;
 import fr.wildcodeschool.goodFather.repositories.RoomRepository;
 import fr.wildcodeschool.goodFather.repositories.TaskRepository;
 import fr.wildcodeschool.goodFather.repositories.TypologyRepository;
+import fr.wildcodeschool.goodFather.repositories.UserRepository;
 import fr.wildcodeschool.goodFather.repositories.WorkRepository;
 
 @Controller
@@ -54,34 +57,50 @@ public class TaskController {
     @Autowired
     TypologyRepository typologyRepository;
 
+    @Autowired
+    UserRepository userRepository;
+
     @GetMapping("/tasks")
-    public String show(Model model, @RequestParam(value = "message", required = false) String message) {
-        List<Task> tasks = taskRepository.findAll();
+    public String show(Authentication authentication, Model model, @RequestParam(value = "message", required = false) String message) {
+        User currentUser = (User)authentication.getPrincipal();
+        if (currentUser.getRole().equals("ADMIN")) {
+            model.addAttribute("isAdmin", true);
+            List<Task> tasks = taskRepository.findTasksByUserId(null);
+            List<Work> works = workRepository.findAll();
+            List<Material> materials = materialRepository.findAll();
+            Collections.sort(tasks);
+            Collections.sort(works);
+            Collections.sort(materials);
+            model.addAttribute("tasks", tasks);
+            model.addAttribute("works", works);
+            model.addAttribute("materials", materials);
+        }
         List<Typology> typologies = typologyRepository.findAll();
-        List<Work> works = workRepository.findAll();
-        List<Material> materials = materialRepository.findAll();
-        Collections.sort(tasks);
         Collections.sort(typologies);
-        Collections.sort(works);
-        Collections.sort(materials);
-        model.addAttribute("tasks", tasks);
         model.addAttribute("typologies", typologies);
-        model.addAttribute("works", works);
-        model.addAttribute("materials", materials);
+        model.addAttribute("user", currentUser);
         model.addAttribute("message", message);
         return "admin/task";
     }
 
     @GetMapping("/tasks/{id}")
-    public String read(@PathVariable Long id, Model model) {
+    public String read(Authentication authentication, @PathVariable Long id, Model model) {
         Task task = taskRepository.findById(id).get();
-        model.addAttribute("task", task);
-        return "admin/task-edit";
+        User currentUser = (User)authentication.getPrincipal();
+        Boolean adminCheck = currentUser.getRole().equals("ADMIN") && task.getUserId() == null;
+        Boolean partnerCheck = currentUser.getId().equals(task.getUserId());
+        if (adminCheck || partnerCheck) {
+            model.addAttribute("task", task);
+            return "admin/task-edit";
+        } else {
+            return "redirect:/error";
+        }
     }
 
     @PostMapping("/tasks")
     public String create(
         Model model,
+        Authentication authentication,
         @RequestParam(required = false) Long workId,
         @RequestParam(required = false) Long materialId,
         @RequestParam(required = false) Long typologyId,
@@ -94,12 +113,16 @@ public class TaskController {
             redirectAttributes.addAttribute("message", "invalide");
         }
         else {
-            if (taskRepository.findTaskByWorkIdAndMaterialIdAndTypologyId(workId, materialId, typologyId) == null) {
+            if (taskRepository.findTaskByWorkIdAndMaterialIdAndTypologyIdAndUserId(workId, materialId, typologyId, null) == null) {
                 Material material = materialRepository.getOne(materialId);
                 Work work = workRepository.getOne(workId);
                 Typology typology = typologyRepository.getOne(typologyId);
-                Task task = new Task(price, unit, percentRange, typology, material, work);
+                Task task = new Task(price, unit, percentRange, typology, material, work, null);
                 task = taskRepository.save(task);
+                for (User user : userRepository.findByRole("PARTNER")) {
+                    task = new Task(price, unit, percentRange, typology, material, work, user.getId());
+                    taskRepository.save(task);
+                }
                 redirectAttributes.addAttribute("message", "success");
             } else {
                 redirectAttributes.addAttribute("message", "doublon");
@@ -116,7 +139,7 @@ public class TaskController {
         @ModelAttribute Material material,
         @RequestParam double quantityValue
     ) {
-        Task task = taskRepository.findTaskByWorkIdAndMaterialIdAndTypologyId(work.getId(), material.getId(), typology.getId());
+        Task task = taskRepository.findTaskByWorkIdAndMaterialIdAndTypologyIdAndUserId(work.getId(), material.getId(), typology.getId(), room.getProject().getSourceId());
         Quantity quantity = new Quantity(room, task, quantityValue);
         if (quantityRepository.findQuantityByRoomIdAndTaskId(room.getId(), task.getId()) == null) {
             Project project = room.getProject();
@@ -131,7 +154,16 @@ public class TaskController {
 
     @DeleteMapping("/tasks/{id}")
     public String delete(@PathVariable Long id, RedirectAttributes redirectAttributes) {
-        taskRepository.deleteById(id);
+        Task task = taskRepository.findById(id).get();
+        List<Task> tasks = taskRepository.findTasksByWorkIdAndMaterialIdAndTypologyId(
+            task.getWork().getId(), 
+            task.getMaterial().getId(),
+            task.getTypology().getId()
+        );
+        for (Task taskToDelete : tasks) {
+            taskToDelete.deleteQuantities();
+            taskRepository.deleteById(taskToDelete.getId());
+        }
         redirectAttributes.addAttribute("message", "delete");
         return "redirect:/tasks";
     }
