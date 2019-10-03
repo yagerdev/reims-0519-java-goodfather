79b6e713dac2cd2590a2a0c913f8dc8d736ddd44
Furkan Celik <c.furkan.contact@gmail.com>
2019-10-03 10:55:41 +0200

    add controller for reset password by admin

--- src/main/java/fr/wildcodeschool/goodFather/controllers/ResetController.java
+++ src/main/java/fr/wildcodeschool/goodFather/controllers/ResetController.java
@@ -1,14 +1,41 @@
 package fr.wildcodeschool.goodFather.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import fr.wildcodeschool.goodFather.entities.User;
+import fr.wildcodeschool.goodFather.repositories.UserRepository;
+@Controller
 public class ResetController{
 
+    @Autowired
+    UserRepository userRepository;
 
-    @GetMapping("/reset")
-    public String edit() {
-      
-        return "admin/user-edit";
+    @GetMapping("/reset/{id}")
+    public String edit(
+        @PathVariable Long id,
+        User user
+    ) {
+        return "admin/reset-password";
     }
 
+    @PutMapping("/reset/{id}")
+    public String resetPassword(
+        User user,
+        @PathVariable Long id,
+        @RequestParam("password") String password,
+        RedirectAttributes redirectAttributes
+        ) {
+        User changeUserPass = userRepository.findById(id).get();
+
+        changeUserPass.setPassword(password);
+        userRepository.save(changeUserPass);
+        redirectAttributes.addAttribute("message", "edit");
+        return"redirect:/users/"+id;
+    }
 }
